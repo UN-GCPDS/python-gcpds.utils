8eb2102ee69a386dbf806e5bb658797530e1ef48
Yeison <yeison.eng@gmail.com>
2020-07-31 17:57:40 -0500

    Version 0.1a0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='gcpds',
-    version='0',
+    version='0.1a0',
     packages=['gcpds'],
 
     author='Yeison Cardona',
