381ecb3b82ba0dbb14bbda9bb48b843a07dba2ac
Yeison <yeison.eng@gmail.com>
2020-07-31 20:57:30 -0500

    Prevent to change the original epoch.

--- gcpds/utils/mne_handler.py
+++ gcpds/utils/mne_handler.py
@@ -2,13 +2,14 @@ import numpy as np
 
 
 # ----------------------------------------------------------------------
-def apply_along_axis(func1d, axis, epochs):
+def apply_along_epochs(func1d, epochs):
     """"""
+    epochs_ = epochs.copy()
 
-    data = np.apply_along_axis(func1d, axis, epochs)
+    data = np.apply_along_axis(func1d, 2, epochs_)
 
     new_fs = data.shape[2] / \
-        (epochs.get_data().shape[2] / epochs.info['sfreq'])
-    epochs.resample(new_fs, npad='auto')
-    epochs._data = data.astype(epochs._data.dtype)
-    return epochs
+        (epochs_.get_data().shape[2] / epochs_.info['sfreq'])
+    epochs_.resample(new_fs, npad='auto')
+    epochs_._data = data.astype(epochs_._data.dtype)
+    return epochs_
