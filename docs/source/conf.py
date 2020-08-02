ef6ef1529ac8a7302788163b12cf9a563d7db4b8
Yeison <yeison.eng@gmail.com>
2020-08-01 19:53:58 -0500

    Update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -120,8 +120,8 @@ autodoc_mock_imports = [
 
 todo_include_todos = True
 
-html_logo = '_static/logo.svg'
-html_favicon = '_static/favico.ico'
+# html_logo = '_static/logo.svg'
+# html_favicon = '_static/favico.ico'
 
 
 def setup(app):
