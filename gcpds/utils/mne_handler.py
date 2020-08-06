cebf5b2554df9f2944104100eee913d624a303c6
Yeison <yeison.eng@gmail.com>
2020-08-05 19:52:32 -0500

    Add function to fit montage

--- gcpds/utils/mne_handler.py
+++ gcpds/utils/mne_handler.py
@@ -1,4 +1,6 @@
 import numpy as np
+import mne
+import pandas as pd
 
 
 # ----------------------------------------------------------------------
@@ -15,3 +17,25 @@ def apply_along_epochs(func1d, epochs):
     # epochs_._data = data - data.mean(axis=2)[..., np.newaxis]
 
     return epochs_
+
+
+# ----------------------------------------------------------------------
+def get_best_montage(channels):
+    """"""
+    data = {}
+    target = set(channels)
+
+    for montage_name in mne.channels.get_builtin_montages():
+        montage = mne.channels.make_standard_montage(montage_name)
+        count = len(target.intersection(set(montage.ch_names)))
+        missings = target.difference(set(montage.ch_names))
+    #     counts.append([count, missings, montage_name])
+
+        data.setdefault('count', []).append(count)
+        data.setdefault('missings', []).append(len(missings))
+        data.setdefault('missings channels', []).append(missings)
+        data.setdefault('montage', []).append(montage_name)
+
+    df = pd.DataFrame.from_dict(data)
+    return df.sort_values('count', ascending=False, ignore_index=True)
+
