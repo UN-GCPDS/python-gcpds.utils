eb89a1b160ad1ac29687807c681eac3d12fe34a6
Yeison <yeison.eng@gmail.com>
2020-12-16 14:58:25 -0500

    Fix channels index

--- gcpds/utils/loaddb/base.py
+++ gcpds/utils/loaddb/base.py
@@ -176,7 +176,7 @@ class Database(metaclass=ABCMeta):
 
         if channels != ALL:
             channels = [self.metadata['channel_names'].index(
-                ch) if isinstance(ch, str) else (ch - 1) for ch in channels]
+                ch) if isinstance(ch, str) else (ch) for ch in channels]
         else:
             channels = list(range(1, len(self.metadata['channel_names']) + 1))
 
