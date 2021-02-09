9725529c23a64b451f7a7bfe9cef760496fa9115
Yeison <yeison.eng@gmail.com>
2021-02-09 14:18:52 -0500

    fix apply_along_epochs

--- gcpds/utils/mne_handler.py
+++ gcpds/utils/mne_handler.py
@@ -8,11 +8,11 @@ def apply_along_epochs(func1d, epochs):
     """"""
     epochs_ = epochs.copy()
 
-    data = np.apply_along_axis(func1d, 2, epochs_)
+    data = np.apply_along_axis(func1d, -1, epochs_._data)
 
-    new_fs = data.shape[2] / \
-        (epochs_.get_data().shape[2] / epochs_.info['sfreq'])
-    epochs_.resample(new_fs, npad='auto')
+    # new_fs = data.shape[2] / \
+        # (epochs_.get_data().shape[2] / epochs_.info['sfreq'])
+    # epochs_.resample(new_fs, npad='auto')
     epochs_._data = data
     # epochs_._data = data - data.mean(axis=2)[..., np.newaxis]
 
