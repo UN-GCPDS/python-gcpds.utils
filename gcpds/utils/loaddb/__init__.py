7564c7b4a05fbc1bb4f397443e91cd86fd75f411
Yeison <yeison.eng@gmail.com>
2021-03-04 20:29:52 -0500

    Update

--- gcpds/utils/loaddb/__init__.py
+++ gcpds/utils/loaddb/__init__.py
@@ -1,4 +1,5 @@
 # BCI2a, GIGA_Laplacian, databases
-from .loaddb import GIGA, BCI2a, HighGamma, BCIilliteracy_MI, BCIilliteracy_ERP, BCIilliteracy_SSVEP, PhysionetMMI
+from .loaddb import GIGA, BCI2a, HighGamma, PhysionetMMI
+from .loaddb import GIGA_MotorImageryExecution, BCI_CIV_2a, HighGamma_MI, PhysioNet_MotorImageryExecution, GIGA_BCI_ERP, GIGA_BCI_MI, GIGA_BCI_SSVEP
 
-available_databases = 'GIGA BCI2a HighGamma BCIilliteracy_MI BCIilliteracy_ERP BCIilliteracy_SSVEP PhysionetMMI'.split()
+available_databases = 'GIGA_MotorImageryExecution BCI_CIV_2a HighGamma_MI PhysioNet_MotorImageryExecution GIGA_BCI_ERP GIGA_BCI_MI GIGA_BCI_SSVEP'.split()
