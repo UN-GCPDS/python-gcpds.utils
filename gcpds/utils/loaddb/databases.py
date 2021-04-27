1952ce1f4c5b6ed43eb06e977085b5277e9fd714
Yeison <yeison.eng@gmail.com>
2021-04-27 13:58:41 -0500

    added keep_runs_separated

--- gcpds/utils/loaddb/databases.py
+++ gcpds/utils/loaddb/databases.py
@@ -8,7 +8,7 @@ fids = json.load(
 giga = {
     'channel_names': ['Fp1', 'AF7', 'AF3', 'F1', 'F3', 'F5', 'F7', 'FT7', 'FC5', 'FC3', 'FC1', 'C1', 'C3', 'C5', 'T7', 'TP7', 'CP5', 'CP3', 'CP1', 'P1', 'P3', 'P5', 'P7', 'P9', 'PO7', 'PO3', 'O1', 'Iz', 'Oz', 'POz', 'Pz', 'CPz', 'Fpz', 'Fp2', 'AF8', 'AF4', 'AFz', 'Fz', 'F2', 'F4', 'F6', 'F8', 'FT8', 'FC6', 'FC4', 'FC2', 'FCz', 'Cz', 'C2', 'C4', 'C6', 'T8', 'TP8', 'CP6', 'CP4', 'CP2', 'P2', 'P4', 'P6', 'P8', 'P10', 'PO8', 'PO4', 'O2'],
     'classes': ['left hand mi', 'right hand mi', 'left hand mm', 'right hand mm'],
-    'non_task_classes': ['resting', 'noise: eye_blinking', 'noise: eye_up_down', 'noise: eye_left_right', 'noise: jaw_clenching', 'noise: head_left_right'],
+    'non_task_classes': ['resting', 'eye_blinking', 'eye_up_down', 'eye_left_right', 'jaw_clenching', 'head_left_right'],
     'sampling_rate': 512,
     'montage': 'standard_1005',
     'tmin': -2,
